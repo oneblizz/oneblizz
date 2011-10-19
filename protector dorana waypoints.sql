cfb02866ac135653b3a39ffec0c258a3c36a4f37
oneblizz <dekanydavid@gmail.com>
2011-10-19 23:40:20 +0200

    Protector Dorana Waypoints

---
+++ protector dorana waypoints.sql
@@ -0,0 +1,89 @@
+/*
+WESTFALL
+Protector Dorana
+Waypoints
+*********************************************************************
+*/
+
+create table if not exists `waypoint_data` (
+	`id` double ,
+	`point` int ,
+	`position_x` float ,
+	`position_y` float ,
+	`position_z` float ,
+	`orientation` float ,
+	`delay` double ,
+	`move_flag` tinyint (1),
+	`action` double ,
+	`action_chance` int ,
+	`wpguid` double 
+); 
+delete from `waypoint_data` where `id`=895320;
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','32','-10515.8','956.547','40.9106','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','31','-10502','953.626','39.6408','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','30','-10488.2','954.618','38.128','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','29','-10480.3','955.397','37.0467','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','28','-10470.7','957.287','35.9499','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','27','-10459.8','960.234','35.1753','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','26','-10452.2','966.017','34.9852','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','25','-10442.4','972.626','35.2603','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','24','-10437.7','976.936','35.0112','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','23','-10433.6','980.894','34.6481','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','22','-10425.1','987.398','33.5591','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','21','-10413.1','994.044','32.3774','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','20','-10407','995.26','31.8264','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','19','-10401.2','993.329','31.5867','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','18','-10397.3','991.321','31.4567','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','17','-10392.6','988.018','31.2882','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','16','-10384.3','983.276','31.1286','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','15','-10376.1','981.164','31.1242','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','14','-10369.9','980.443','31.1414','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','13','-10364.8','980.568','31.1309','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','12','-10359.2','980.334','31.1439','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','11','-10348.8','980.25','31.1304','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','10','-10342.1','979.681','31.1377','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','9','-10334.8','979.065','31.1309','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','8','-10329.9','978.336','31.1309','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','7','-10326.3','976.813','31.1309','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','6','-10319','973.815','31.1586','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','5','-10313.8','970.149','31.1278','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','4','-10304.8','967.515','31.1277','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','3','-10299','966.196','31.1205','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','2','-10291.9','964.628','31.124','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','1','-10283.2','966.329','31.1261','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','36','-10568.3','977.347','40.0462','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','35','-10552.2','969.142','41.2934','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','34','-10540.9','964.744','41.3919','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','33','-10529.2','960.855','40.9002','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','37','-10577.2','981.681','38.6038','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','38','-10592.6','988.81','36.7993','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','39','-10604','994.482','35.0826','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','40','-10622.8','1004.19','32.691','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','41','-10632.8','1010.16','32.1143','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','42','-10645.3','1018.24','31.4055','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','43','-10653.5','1023.64','31.6458','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','44','-10663.1','1029.79','32.6951','0','8000','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','45','-10664','1024.22','32.4747','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','46','-10652.1','1015.78','31.5075','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','47','-10637.8','1006.76','32.1692','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','48','-10622.6','999.208','32.8282','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','49','-10607.1','992.402','35.0107','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','50','-10591.6','985.677','37.2149','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','51','-10580','980.382','38.3787','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','52','-10564.7','973.281','40.6745','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','53','-10549.4','965.984','41.3457','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','54','-10533.7','959.7','41.0805','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','55','-10521.5','953.822','41.2327','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','56','-10505.6','950.946','40.1717','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','57','-10490','950.148','38.4684','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','58','-10470.3','952.587','36.1095','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','59','-10447.8','964.914','35.2739','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','60','-10430.6','980.342','34.6206','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','61','-10404.1','989.755','32.0454','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','62','-10386.2','981.297','31.1277','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','63','-10379.3','979.021','31.1678','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','64','-10357','978.086','31.1305','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','65','-10331.2','975.671','31.13','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','66','-10314.7','968.805','31.1343','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','67','-10296.9','962.686','31.1303','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('895320','68','-10279.7','963.66','31.128','0','0','0','0','100','0');
